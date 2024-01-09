fe640dfa8c5e2dc5706c9ce6a0e5e9d53a7d0aa6
VenMen <lizaivana55555@gmail.com>
2024-01-09 19:48:56 +0500

    Added some new

---
+++ Assets/Scripts/andsomenew.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class andsomenew: MonoBehaviour
+{
+    public Slider slider1;
+    public Slider slider2;
+    public TextMeshProUGUI CounterX;
+    public TextMeshProUGUI CounterY;
+    public int counrX;
+    public int counrY;
+    public int xForMaze;
+    public int yForMaze;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CounterX.text = counrX.ToString("0");
+        CounterY.text = counrY.ToString("0");
+        slider1.onValueChanged.AddListener((v) => { CounterX.text = v.ToString("0"); });
+        slider2.onValueChanged.AddListener((v) => { CounterY.text = v.ToString("0"); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xForMaze = (int)(slider2.value);
+        yForMaze = (int)(slider1.value);
+        Save();
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("XMaze", xForMaze);
+        PlayerPrefs.SetInt("YMaze", yForMaze);
+        PlayerPrefs.Save();
+    }
+}
